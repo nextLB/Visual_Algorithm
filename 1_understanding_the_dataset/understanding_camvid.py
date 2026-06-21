17a2fc56286e98e8f3bf4c0d60a4e6ac425c5681
next <364574103@qq.com>
2026-06-21 16:42:12 +0800

    update

--- 1_understanding_the_dataset/understanding_camvid.py
+++ 1_understanding_the_dataset/understanding_camvid.py
@@ -8,7 +8,9 @@ READ_BASE_DATA_DIR_PATH = '/home/next/桌面/Open_Source_Dataset_For_Visual_Algo
 
 SAVE_BASE_DATA_DIR_PATH = '/home/next/桌面/Open_Source_Dataset_For_Visual_Algorithms/General_Visual_Algorithm_Dataset/'
 
-
+TRAIN_DIR = 'train'
+TRAIN_LABEL_DIR = 'train_labels'
+TRAIN_MASK_DIR = 'trainMasks'
 
 
 
@@ -21,6 +23,8 @@ SAVE_BASE_DATA_DIR_PATH = '/home/next/桌面/Open_Source_Dataset_For_Visual_Algo
 if __name__ == '__main__':
 
     print('开始运行脚本程序,有助于详细理解Camvid数据集')
+
+    # 排序图像数据
     
 
 
