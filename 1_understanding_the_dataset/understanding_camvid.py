fa050a233765e14f3c6cf50cc61eb61c2e855bdd
next <364574103@qq.com>
2026-06-22 13:53:08 +0800

    temp update

--- 1_understanding_the_dataset/understanding_camvid.py
+++ 1_understanding_the_dataset/understanding_camvid.py
@@ -2,6 +2,13 @@
     create on 2026.06.18
 """
 
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
 
 
 READ_BASE_DATA_DIR_PATH = '/home/next/桌面/Open_Source_Dataset_For_Visual_Algorithms/Camvid/'
@@ -24,7 +31,29 @@ if __name__ == '__main__':
 
     print('开始运行脚本程序,有助于详细理解Camvid数据集')
 
-    # 排序图像数据
+    # 以训练数据集为例，进行加载与分析
+    trainImagePath = os.path.join(READ_BASE_DATA_DIR_PATH, TRAIN_DIR)
+    imageFiles = sorted(os.listdir(trainImagePath))
+
+    # 逐张加载与展示图像与标签数据等
+    for singleImageFile in imageFiles:
+        baseName = os.path.splitext(singleImageFile)[0]
+        singleLabelFile = baseName + "_L.png"
+        singleMaskFile = baseName + "_L.png"
+        singleImagePath = os.path.join(READ_BASE_DATA_DIR_PATH, TRAIN_DIR, singleImageFile)
+        singleLabelPath = os.path.join(READ_BASE_DATA_DIR_PATH, TRAIN_LABEL_DIR, singleLabelFile)
+        singleMaskPath = os.path.join(READ_BASE_DATA_DIR_PATH, TRAIN_MASK_DIR, singleMaskFile)
+
+        # 读取原图  
+        imageBGR = cv2.imread(singleImagePath)
+        if imageBGR is None:
+            sys.exit(f"无法读取: {singleImagePath}")
+        imageRGB = cv2.cvtColor(imageBGR, cv2.COLOR_BGR2RGB)
+        print(imageRGB)
+
+        # 读取彩色标签图    三通道彩色
+        labelBGR = cv2.imread(singleLabelPath)
+        
     
 
 
