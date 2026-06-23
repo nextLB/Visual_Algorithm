83e06065a81bcd29a1e733533ed4bcaba83e8048
next <364574103@qq.com>
2026-06-23 14:47:19 +0800

    update create html

--- picture_video_what_it_is/visual_algorithm_website/main_interface/views.py
+++ picture_video_what_it_is/visual_algorithm_website/main_interface/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# 首页视图
 def home(request):
-    return HttpResponse("""
-        <h1>🎨 视觉算法演示平台</h1>
-        <p>欢迎来到图像与视频算法测试界面！</p>
-        <p>后续将在这里集成图像分类、目标检测等功能。</p>
-    """)
+    return render(request, 'home.html')
