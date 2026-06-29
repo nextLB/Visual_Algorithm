942ad9e844fea1640efa189fb187e36b4973c395
next <364574103@qq.com>
2026-06-29 13:49:56 +0800

    up

--- C++/loading_and_understanding_dataset/VOC_datasets/main.cpp
+++ C++/loading_and_understanding_dataset/VOC_datasets/main.cpp
@@ -1,6 +1,22 @@
 // create on 2026.06.26
 
 
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+
+int main() {
+
+    string baseDir = "/home/next/桌面/Open_Source_Dataset_For_Visual_Algorithms/VOC/";
+
+    cout << baseDir << endl;
+
+
+
+
+    return 0;
+}
 
 
